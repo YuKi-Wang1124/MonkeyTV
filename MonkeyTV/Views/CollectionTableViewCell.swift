a777e1497320a1ec9555dabfa1e0aad66283fadd
kiki <kikiwang3185@gmail.com>
2023-09-16 11:20:54 +0800

    add Home Page Collection View cell

--- MonkeyTV/Views/CollectionTableViewCell.swift
+++ MonkeyTV/Views/CollectionTableViewCell.swift
@@ -113,6 +113,10 @@ extension CollectionTableViewCell: UICollectionViewDelegateFlowLayout {
                         insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        
+    }
 }
 
 enum OneSection {
