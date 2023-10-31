4ea02685f6aa72730bca3408c056862cc76aa48f
kiki <kikiwang3185@gmail.com>
2023-10-31 17:42:28 +0800

    dequeueReusableCell

--- MonkeyTV/Controllers/FavoriteViewController.swift
+++ MonkeyTV/Controllers/FavoriteViewController.swift
@@ -21,34 +21,27 @@ class FavoriteViewController: UIViewController, UITableViewDelegate {
     private var tableViewDataSource: UITableViewDiffableDataSource<OneSection, ShowData>!
     
     // MARK: - Life Cycle
-    
-    override func viewWillAppear(_ animated: Bool) {
-        Task { await updateTableViewDataSource() }
+    override func loadView() {
+        view = self.tableView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
         tableView.delegate = self
-        setUI()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        Task { await updateTableViewDataSource() }
+    }
     // MARK: - Update TableView DataSource
     
-    func updateTableViewDataSource() async {
+    private func updateTableViewDataSource() async {
         tableViewDataSource =
         UITableViewDiffableDataSource<OneSection, ShowData>(
             tableView: tableView) { tableView, indexPath, itemIdentifier in
-                
-                let cell =
-                tableView.dequeueReusableCell(
-                    withIdentifier: ShowTableViewCell.identifier,
-                    for: indexPath) as? ShowTableViewCell
-                guard let cell = cell else { return UITableViewCell() }
-                
+                let cell: ShowTableViewCell = tableView.dequeueReusableCell(for: indexPath)
                 cell.showImageView.loadImage(itemIdentifier.showImage)
                 cell.showNameLabel.text = itemIdentifier.showName
-                
                 return cell
             }
         
@@ -61,19 +54,6 @@ class FavoriteViewController: UIViewController, UITableViewDelegate {
             tableViewSnapshot.appendItems(showArray, toSection: .main)
             await tableViewDataSource.apply(tableViewSnapshot)
         }
-    }
-    
-    private func setUI() {
-        view.addSubview(tableView)
-        view.backgroundColor = .baseBackgroundColor
-        tableView.backgroundColor = .baseBackgroundColor
-
-        NSLayoutConstraint.activate([
-            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
